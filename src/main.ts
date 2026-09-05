d832ac90701c87bdf0d28b44cf3e9971d26e5b72
mannyivfouy <mannyivfouy@gmail.com>
2026-09-05 09:22:29 +0700

    learn about error handler and global interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { TransformInterceptor } from './utils/transform.interceptor.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new TransformInterceptor())
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { LoggerService } from './user.logger.js';
 import { CreateUserDto } from './dto/create-user.dto.js';
 import { UpdateUserDto } from './dto/update-user.dto.js';
@@ -26,9 +26,13 @@ export class UserService {
   }
 
   findOneUser(id: number) {
-    this.logger.log(`Finding User ${id}`);
+    const user = this.users.find((user) => user.id === id);
 
-    return this.users.find((user) => user.id === id) ?? null;
+    if (!user) {
+      throw new NotFoundException('User Not found');
+    }
+
+    return user;
   }
 
   createUse(dto: CreateUserDto) {
